8d0d4938a26bf9a9182c8e22e9b5c68fd893048c
Daniel Skinner <daniel@dasa.cc>
2013-02-14 09:39:25 -0600

    response helper

---
+++ src/org/tsg/web/WebResponse.java
@@ -0,0 +1,54 @@
+package org.tsg.web;
+
+import android.content.Context;
+import android.os.Bundle;
+
+public class WebResponse {
+
+	private Context mContext;
+	private int mResultCode;
+	private Bundle mResultData;
+
+	public WebResponse(Context context, int resultCode, Bundle resultData) {
+		mContext = context;
+		mResultCode = resultCode;
+		mResultData = resultData;
+	}
+
+	public Context getContext() {
+		return mContext;
+	}
+
+	public int getStatus() {
+		return mResultCode;
+	}
+
+	public boolean isRunning() {
+		return mResultCode == WebReceiver.STATUS_RUNNING;
+	}
+
+	public boolean isFinished() {
+		return mResultCode == WebReceiver.STATUS_FINISHED;
+	}
+
+	public boolean isException() {
+		return mResultCode == WebReceiver.STATUS_ERROR;
+	}
+
+	public String getRequestKey() {
+		return mResultData.getString(WebReceiver.REQUEST_KEY);
+	}
+
+	public String getString() {
+		return WebService.getResponseString(mContext, getRequestKey());
+	}
+
+	public byte[] getBytes() {
+		return WebService.getResponseBytes(mContext, getRequestKey());
+	}
+
+	public Exception getException() {
+		return (Exception) mResultData.getSerializable(WebReceiver.RESPONSE_EXCEPTION);
+	}
+
+}
